7d66783eabe4c6e460c9dc094a418ba4051511ed
kundankumarjha <it.kundanmca@gmail.com>
2019-06-12 22:31:19 +0530

    Add files via upload

--- job/src/com/example/job/Main.java
+++ job/src/com/example/job/Main.java
@@ -1,18 +1,23 @@
 package com.example.job;
 
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) throws Exception {
-		List<Job> jobs = new ArrayList<>();
-		jobs.add(new Job("a", null));
-		jobs.add(new Job("b", "c"));
-		jobs.add(new Job("c", "f"));
-		jobs.add(new Job("d", "a"));
-		jobs.add(new Job("e", "b"));
-		jobs.add(new Job("f", null));
-
+		Scanner scanner = new Scanner(new InputStreamReader(System.in));
+		System.out.println("Enter no. of jobs : ");
+		int jobCount = Integer.parseInt(scanner.nextLine());
+		List<Job> jobs = new ArrayList<>(jobCount);
+		for(int i=1 ; i<= jobCount; i++) {
+			System.out.println("Enter "+ i +" job name : ");
+			String jobName = scanner.nextLine();
+			System.out.println("Enter "+ i +" job dependency : ");
+			String jobDependency = scanner.nextLine();
+			jobs.add(new Job(jobName, jobDependency));
+		}
 		OrderJobs orderJobs = new OrderJobs(jobs);
 		System.out.println(orderJobs.execute());
 	}
